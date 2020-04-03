f8890a4f176476a17a36169d482df4eb5313105c
kingbabar8123 <54685065+kingbabar8123@users.noreply.github.com>
2020-04-02 22:12:57 -0500

    slideshow

--- js/script.js
+++ js/script.js
@@ -1,41 +1,66 @@
-function init(){
-//add your javascrip between these two lines of code
+$('.slider').each(function() {              // For every slider
+  var $this   = $(this);                    // Current slider
+  var $group  = $this.find('.slide-group'); // Get the slide-group (container)
+  var $slides = $this.find('.slide');       // Create jQuery object to hold all slides
+  var buttonArray  = [];                    // Create array to hold navigation buttons
+  var currentIndex = 0;                     // Hold index number of the current slide
+  var timeout;                              // Sets gap between auto-sliding
 
-document.getElementById('button').addEventListener('click', function(){
-	var pw = document.getElementById('password').value;
-	var pw1 = document.getElementById('input_float_pw').value;
-	var email = document.getElementById('email').value;
-	var plength = document.getElementById('password').value.length;
-	var regex = /^[A-Za-z0-9 ]+$/;
-	var isValid = regex.test(pw);
-	
-	if ( pw != pw1) {
-		alert('Your passwords dont match please try again');
-		document.getElementById("myform").addEventListener("click", function(event){
-		event.preventDefault()
-	});
-	}else if (isValid) {
-		alert("Your password must contain at least one special character to proceed");
-		document.getElementById("myform").addEventListener("click", function(event){
-		event.preventDefault()
-	});
-    } else if (plength==0){
-		alert("Your Password cannot be blank please provide a suitable password.");
-		document.getElementById("myform").addEventListener("click", function(event){
-		event.preventDefault()
-	});
-	} else if (plength<=7){
-		alert("Your Password is too short! It must be at least 8 characters, please try again.");
-		document.getElementById("myform").addEventListener("click", function(event){
-		event.preventDefault()
-	});
-	} else {
-		alert('Your password is acceptable');
-		document.getElementById("myform").submit();
-	}
+  function move(newIndex) {          // Creates the slide from old to new one
+    var animateLeft, slideLeft;      // Declare variables
+
+    advance();                       // When slide moves, call advance() again
+
+    // If it is the current slide / animating do nothing
+    if ($group.is(':animated') || currentIndex === newIndex) {  
+      return;
+    }
+
+    buttonArray[currentIndex].removeClass('active'); // Remove class from item
+    buttonArray[newIndex].addClass('active');        // Add class to new item
+
+    if (newIndex > currentIndex) {   // If new item > current
+      slideLeft = '100%';            // Sit the new slide to the right
+      animateLeft = '-100%';         // Animate the current group to the left
+    } else {                         // Otherwise
+      slideLeft = '-100%';           // Sit the new slide to the left
+      animateLeft = '100%';          // Animate the current group to the right
+    }
+    // Position new slide to left (if less) or right (if more) of current
+    $slides.eq(newIndex).css( {left: slideLeft, display: 'block'} );
+
+    $group.animate( {left: animateLeft}, function() {    // Animate slides and
+      $slides.eq(currentIndex).css( {display: 'none'} ); // Hide previous slide      
+      $slides.eq(newIndex).css( {left: 0} ); // Set position of the new item
+      $group.css( {left: 0} );               // Set position of group of slides
+      currentIndex = newIndex;               // Set currentIndex to the new image
+    });
+  }
+
+  function advance() {                     // Used to set 
+    clearTimeout(timeout);                 // Clear previous timeout
+    timeout = setTimeout(function() {      // Set new timer
+      if (currentIndex < ($slides.length - 1)) { // If slide < total slides
+        move(currentIndex + 1);            // Move to next slide
+      } else {                             // Otherwise
+        move(0);                           // Move to the first slide
+      }
+    }, 4000);                              // Milliseconds timer will wait
+  }
+
+  $.each($slides, function(index) {
+    // Create a button element for the button
+    var $button = $('<button type="button" class="slide-btn">&bull;</button>');
+    if (index === currentIndex) {    // If index is the current item
+      $button.addClass('active');    // Add the active class
+    }
+    $button.on('click', function() { // Create event handler for the button
+      move(index);                   // It calls the move() function
+    }).appendTo('.slide-buttons');   // Add to the buttons holder
+    buttonArray.push($button);       // Add it to the button array
+  });
+
+  advance();                          // Script is set up, advance() to move it
+
+
 });
-
-}
-
- 
-window.addEventListener('load', init);
